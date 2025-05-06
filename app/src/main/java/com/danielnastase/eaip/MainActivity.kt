b15941ba363117832d4b58b73c820d4c18f1b19d
RoundRaccoon <ddaninn01@gmail.com>
2025-05-06 16:43:14 +0300

    Setup hierarchy

--- app/src/main/java/com/danielnastase/eaip/MainActivity.kt
+++ app/src/main/java/com/danielnastase/eaip/MainActivity.kt
@@ -3,45 +3,13 @@ package com.danielnastase.eaip
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.danielnastase.eaip.ui.theme.EaipTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            EaipTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
-    }
-}
+        window.statusBarColor = getColor(R.color.black)
+        window.navigationBarColor = 0xFF17181D.toInt()
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    EaipTheme {
-        Greeting("Android")
+        setContent{ GooderScreen() }
     }
 }
